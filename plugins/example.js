f9106d6168ced63427a672f949d43b2ad68beb9f
Gefocheh <kulpin.giorgio@gmail.com>
2026-08-25 17:20:59 +0200

    add new commands to plugin
    added tpp (teleport to player)
    added players (list all online players)

--- plugins/example.js
+++ plugins/example.js
@@ -9,6 +9,34 @@ module.exports.init = api => {
       api.players.sendMessage(playerId, 'Teleported to spawn');
     }
   });
+  api.registerCommand({
+    name: 'tpp',
+    description: 'teleport to player',
+    handler(playerId, args) {
+      const target = api.players.get(args);
+      if (!target) {
+        api.players.sendMessage(playerId, "Player not found");
+      } else {
+        api.players.correctPos(playerId, target.x, target.y, target.z);
+        api.players.sendMessage(playerId, "Teleported to " + args);
+        api.players.sendMessage(args, "Player ${playerId} teleported to you");
+
+      }
+
+    }
+  });
+  api.registerCommand({
+    name: 'players',
+    description: 'list all players currently connected to server',
+    handler(playerId) {
+      const playerObjectsArray = api.players.getAll()
+      var playerList
+      for (let i = 0; i <= playerObjectsArray.length; i ++) {
+        playerList[i] = playerObjectsArray[i].nickname;
+      }
+      api.players.sendMessage("online players: " + playerList)
+    }
+  })
 
   api.on('playerJoin', e => {
     api.players.sendMessage(e.playerId, 'Welcome!');
